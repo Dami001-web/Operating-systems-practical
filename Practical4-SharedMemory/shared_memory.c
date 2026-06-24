6fe40d7dc13595eed4c3d336574e000c53d6d768
Dami001-web <idamemmanuel002@gmail.com>
2026-06-24 17:24:16 +0100

    Create shared_memory.c

---
+++ Practical4-SharedMemory/shared_memory.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <string.h>
+#include <unistd.h>
+
+int main() {
+
+    key_t key = 1234;
+
+    int shmid = shmget(key, 1024, 0666 | IPC_CREAT);
+
+    char *str = (char *)shmat(shmid, NULL, 0);
+
+    strcpy(str, "Hello from Shared Memory!");
+
+    printf("Written Data: %s\n", str);
+
+    shmdt(str);
+
+    shmctl(shmid, IPC_RMID, NULL);
+
+    return 0;
+}
